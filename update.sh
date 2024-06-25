a0b38ba795f363762c9301e761d65343161f19b2
Rajasimman S <srajasimman@gmail.com>
2024-06-25 08:02:48 +0530

    converted as ansible role

--- update.sh
+++ update.sh
@@ -3,5 +3,4 @@
 if [[ -z "$@" ]]; then
     echo "Usage: $0 [dev|qa|uat|prod] [noc|devops|engg|sa]" && exit 1
 fi
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i "hosts/$1" user_management.yml --extra-vars "@users/$2.yml"
-
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i "hosts/$1" ansible/users.yml --extra-vars "@users/$2.yml"
